8014f72fed0a58b7ef734e38af22c1d400d9c173
benank <terkolskie@gmail.com>
2021-09-16 19:53:29 -0700

    Add TFJS scripts and camera

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,87 @@
 <script>
+	import { onMount } from "svelte";
+
 	export let name;
+
+	let camera;
+
+	let mounted = false;
+	let tfjsReady = false;
+	onMount(() => {
+		mounted = true;
+		if (tfjsReady) {
+			TFJSReadyAndMounted();
+		}
+
+		/*navigator.mediaDevices
+			.enumerateDevices()
+			.then(function (devices) {
+				console.table(devices);
+				devices.forEach(function (device) {
+					console.log(
+						device.kind +
+							": " +
+							device.label +
+							" id = " +
+							device.deviceId
+					);
+				});
+			})
+			.catch(function (err) {
+				console.log(err.name + ": " + err.message);
+			});*/
+	});
+
+	let tfjs_num_loaded = 0;
+	function loadTFJS() {
+		tfjs_num_loaded++;
+
+		if (tfjs_num_loaded == 4) {
+			tfjsReady = true;
+			if (mounted) {
+				TFJSReadyAndMounted();
+			}
+		}
+	}
+
+	function TFJSReadyAndMounted() {
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+			// TEMP: only use this device id in localhost for correct camera
+			// or keep because it still allows you to choose camera if this one doesn't exist
+				.getUserMedia({ video: {deviceId: "8Icmu55xfG41JYbpcguRkmmGPzMEPXjeW6Ksp30PA10="} })
+				.then(function (stream) {
+					camera.srcObject = stream;
+				})
+				.catch(function (err0r) {
+					console.log("Something went wrong!");
+				});
+		}
+	}
 </script>
+
+<svelte:head>
+	<script
+		src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-core"
+		on:load={loadTFJS}></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-converter"
+		on:load={loadTFJS}></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-webgl"
+		on:load={loadTFJS}></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection"
+		on:load={loadTFJS}></script>
+</svelte:head>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<video autoplay="true" bind:this={camera} />
 </main>
 
 <style>
