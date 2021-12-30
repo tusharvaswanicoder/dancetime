7a26ed2f385356a9d3af386d0da6e6da336e2cf5
benank <ben@b-a.dev>
2021-12-29 20:06:43 -0800

    Add basic connection to mysql

---
+++ server/AzMySQLManager.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DATABASE_NAME = process.env.NODE_ENV == 'production' ?
+    'dancetime_prod' : 'dancetime_dev';
+
+const conn = mysql.createConnection({ 
+    host: process.env.MYSQL_DB_HOST, 
+    user: process.env.MYSQL_DB_USER, 
+    password: process.env.MYSQL_DB_PW, 
+    database: DATABASE_NAME, 
+    port: process.env.MYSQL_DB_PORT,
+    ssl: {ca: fs.readFileSync(path.join(__dirname, `/ssl/${process.env.MYSQL_DB_SSL_CERT}.crt.pem`))}
+})
+
+conn.connect(
+    function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+        //    queryDatabase();
+    }
+});
