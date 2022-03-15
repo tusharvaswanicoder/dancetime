5d9ce3a710a7149c99b3f95e9674fc08ff763baf
benank <terkolskie@gmail.com>
2022-03-15 13:04:04 -0700

    Up

--- api/api-auth-login/index.mjs
+++ api/api-auth-login/index.mjs
@@ -9,4 +9,5 @@ export default async function (context, req) {
     // User arrives here with a magic link
     await MagicLinkLogin(context, req);
     context.log(context);
+    context.log(context.res.cookies);
 };
